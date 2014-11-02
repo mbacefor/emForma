b9968f1211f2db8c8f83e23ac63912f033b881b1
mbacefor <mbacefor@gmail.com>
2014-11-02 16:42:29 -0200

    Projeto do cliente

---
+++ clientWSEmForma/src/main/java/br/edu/fa7/emforma/ws/client/MyServiceClient.java
@@ -0,0 +1,14 @@
+package br.edu.fa7.emforma.ws.client;
+
+import br.edu.fa7.emforma.ws.server.EmFormaService;
+import br.edu.fa7.emforma.ws.server.EmFormaServiceImplService;
+
+public class MyServiceClient {
+	public static void main(String[] args) {
+		EmFormaServiceImplService serviceImpl = new EmFormaServiceImplService();
+		EmFormaService service = serviceImpl.getEmFormaServiceImplPort();
+		System.out.println(service.reverse("123456789"));
+		System.out.println(service.login("Teste", "Hello") + "Teste1");
+
+	}
+}
